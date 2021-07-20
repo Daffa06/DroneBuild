b6ad10b4040933c511b43f92516088526439f704
©TwT <vcyzscape@gmail.com>
2021-07-20 13:28:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-git clone https://github.com/vcyzteen/baka -b lineage-18.1-4.19
-cd baka
-curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/MASTER.sh | bash
+git clone https://github.com/Yoga3911/excalibur_hmp
+cd excalibur_hmp
+git remote remove origin
+git remote add origin https://github.com/mrcloudd69/Barong.git
+git branch -M master
+git push -u origin master
