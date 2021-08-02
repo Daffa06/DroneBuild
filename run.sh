c8db40d08be945f3a946f86eaf6afbf44540833b
©TwT <vcyzscape@gmail.com>
2021-08-02 19:43:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-git clone https://github.com/Yoga3911/excalibur_hmp
-cd excalibur_hmp
-git remote remove origin
-git remote add origin https://github.com/mrcloudd69/Barong.git
-git branch -M master
-git push https://$GITHUB_TOKEN@github.com/mrcloudd69/Barong.git master
+git clone https://source.codeaurora.org/quic/la/kernel/msm-4.4 -b kernel.lnx.4.4.r42-rel
+cd msm-4.4
+git checkout -b baka-4.4
+git remote set-url origin https://github.com/Baka-Project/Baka-Msm-4.4.git
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git baka-4.4
