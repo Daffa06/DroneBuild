4e3549891ef3905fb1f02c8c8cc27cbf6f30fe90
©TwT <vcyzscape@gmail.com>
2021-08-02 20:32:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
-git clone https://source.codeaurora.org/quic/la/kernel/msm-4.4 -b kernel.lnx.4.4.r42-rel
-cd msm-4.4
-git checkout -b baka-4.4
-git remote set-url origin https://github.com/Baka-Project/Baka-Msm-4.4.git
-git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git baka-4.4
+git clone https://source.codeaurora.org/quic/la/kernel/msm-4.14 -b kernel.lnx.4.14.r11-rel
+cd msm-4.14
+git checkout -b baka-4.14
+git remote set-url origin https://github.com/Baka-Project/Baka-Msm-4.14.git
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.14.git baka-4.14
+
+cd
+
+git clone https://source.codeaurora.org/quic/la/kernel/msm-4.19 -b kernel.lnx.4.19.r18-rel
+cd msm-4.19
+git checkout -b baka-4.19
+git remote set-url origin https://github.com/Baka-Project/Baka-Msm-4.19.git
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.19.git baka-4.19
