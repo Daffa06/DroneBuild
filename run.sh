4f76e5823932632a50fc2b0dc1ef87f3070a4b5b
©TwT <vcyzscape@gmail.com>
2021-08-03 16:10:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/Baka-Project/Baka-Msm-4.14.git
 cd msm-4.14
 git fetch https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b vayu-r-oss
-git cherry-pick 0c9888aa2373958ae38787ed84b803701f73f0a1
-git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.19 baka-4.19
+git cherry-pick -m1 0c9888aa2373958ae38787ed84b803701f73f0a1
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.14
