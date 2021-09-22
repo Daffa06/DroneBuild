3007a3779bc27bc794640e0d818308fa5d8a58ab
©TwT <vcyzscape@gmail.com>
2021-09-22 11:40:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone git clone https://source.codeaurora.org/quic/la/platform/build/blueprint -b LA.QSSI.11.0.r1-13300-qssi.0
+git clone https://source.codeaurora.org/quic/la/platform/build/blueprint -b LA.QSSI.11.0.r1-13300-qssi.0
 cd blueprint
 git checkout -b eleven
 git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-build-blueprint.git eleven
