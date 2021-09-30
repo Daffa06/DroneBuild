effd41bcf5dc0098faeb27b6a33531207cf3de47
©TwT <vcyzscape@gmail.com>
2021-09-30 08:52:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 git clone https://github.com/Swapnil133609/Android-Kernel-Patches
-git clone https://github.com/Baka-Project/Baka-Kernel-Patches
-cp -r Android-Kernel-Patches/* $HOME/Baka-Kernel-Patches
+cd Android-Kernel-Patches
 rm -rf .git .gitignore README.md
-cd Baka-Kernel-Patches
 git add . && git commit -m "initial" --signoff
 git checkout -b baka
 git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Kernel-Patches.git baka
