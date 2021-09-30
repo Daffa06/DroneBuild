3a817804e3417c0771e5e882b35c92c3abd248eb
©TwT <vcyzscape@gmail.com>
2021-09-30 08:42:22 +0700

    Test

--- run.sh
+++ run.sh
@@ -1,77 +1,13 @@
-git clone https://source.codeaurora.org/quic/la/platform/frameworks/opt/net/ims -b LA.QSSI.11.0.r1-13300-qssi.0
-cd ims
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-frameworks-opt-net-ims.git eleven
+cd $HOME
+git clone https://github.com/Swapnil133609/Android-Kernel-Patches
+git clone https://github.com/Baka-Project/Baka-Kernel-Patches
+cd Android-Kernel-Patches
+cp -r faux123 $HOME/Baka-Kernel-Patches
+cp -r flar2 $HOME/Baka-Kernel-Patches
+cp -r franciscofranco $HOME/Baka-Kernel-Patches
+cp -r labodj $HOME/Baka-Kernel-Patches
+cp -r showp1984 $HOME/Baka-Kernel-Patches
 cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/frameworks/opt/net/wifi -b LA.QSSI.11.0.r1-13300-qssi.0
-cd ims
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-frameworks-opt-net-wifi.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/frameworks/opt/net/telephony -b LA.QSSI.11.0.r1-13300-qssi.0
-cd telephony
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-frameworks-opt-telephony.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/frameworks/opt/net/timezonepicker -b LA.QSSI.11.0.r1-13300-qssi.0
-cd timezonepicker
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-frameworks-opt-timezonepicker.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/zlib -b LA.QSSI.11.0.r1-13300-qssi.0
-cd zlib
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-zlib.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/json-c -b LA.QSSI.11.0.r1-13300-qssi.0
-cd json-c
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-json-c.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/exfat -b LA.QSSI.11.0.r1-13300-qssi.0
-cd exfat
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-exfat.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/icu -b LA.QSSI.11.0.r1-13300-qssi.0
-cd icu
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-icu.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/cldr -b LA.QSSI.11.0.r1-13300-qssi.0
-cd cldr
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-cldr.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/tinycompress -b LA.QSSI.11.0.r1-13300-qssi.0
-cd tinycompress
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-tinycompress.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/nos/host/generic -b LA.QSSI.11.0.r1-13300-qssi.0
-cd generic
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-nos-host-generic.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/wpa_supplicant_8 -b LA.QSSI.11.0.r1-13300-qssi.0
-cd wpa_supplicant_8
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-wpa-supplicant-8.git eleven
-cd ../..
-
-git clone https://source.codeaurora.org/quic/la/platform/external/gptfdisk -b LA.QSSI.11.0.r1-13300-qssi.0
-cd gptfdisk
-git checkout -b eleven
-git push https://$GITHUB_TOKEN@github.com/Weeaboo-Os/weeaboo-external-gptfdisk.git eleven
-cd ../..
+cd Baka-Kernel-Patches
+git checkout -b baka
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Kernel-Patches.git baka
