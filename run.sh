141df21fca1c2a704d58ee455fabf639a86a906b
©TwT <vcyzscape@gmail.com>
2021-09-30 08:58:04 +0700

    P

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/Swapnil133609/Android-Kernel-Patches
+git clone https://github.com/Swapnil133609/Android-Kernel-Patches.git
 cd Android-Kernel-Patches
 rm -rf .git .gitignore README.md
-git add . && git commit -m "initial" --signoff
+git init && git add . && git commit -m "initial" --signoff
 git checkout -b baka
 git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Kernel-Patches.git baka
