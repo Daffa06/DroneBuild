f09840846cd5817a0dde91d68ebf5ceee2cd163b
©TwT <vcyzscape@gmail.com>
2021-10-02 01:24:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # write your code in bellow
 git clone https://github.com/Baka-Project/Baka-Msm-4.4.git
 cd Baka-Msm-4.4
-bash -c "$(wget -O - https://github.com/Baka-Project/Baka-Merged/blob/baka/baka-merged)"
+wget -O https://github.com/Baka-Project/Baka-Merged/blob/baka/baka-merged
+bash baka-merged
 git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git
