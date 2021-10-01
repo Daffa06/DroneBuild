1d6b84001d875617092533e19545a2a42dd4a9d5
©TwT <vcyzscape@gmail.com>
2021-10-01 19:02:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # write your code in bellow
 git clone https://github.com/sivel/speedtest-cli.git
 cd speedtest-cli
-python setup.py install
 pip install setuptools
+python setup.py install
 speedtest-cli --bytes
