b213e53bde7aff16d2a9956131d2afeb9f1b7c42
©TwT <vcyzscape@gmail.com>
2021-10-02 01:15:44 +0700

    Merged

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 # write your code in bellow
-pip install speedtest-cli
-speedtest-cli
+git clone https://github.com/Baka-Project/Baka-Msm-4.4.git
+cd Baka-Msm-4.4
+bash -c "$(wget -O - https://github.com/Baka-Project/Baka-Merged/blob/baka/baka-merged)"
+git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git
