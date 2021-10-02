55757175dc63948eb3376821e55da29bd12959a0
©TwT <vcyzscape@gmail.com>
2021-10-02 14:41:00 +0700

    `Microwave`

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # write your code in bellow
-git clone https://github.com/Baka-Project/Baka-Msm-4.4.git
-cd Baka-Msm-4.4
-git fetch https://source.codeaurora.org/quic/la/kernel/msm-4.4/ LA.UM.8.2.r2-04800-sdm660.0
-git merge FETCH_HEAD
-git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git
+git clone https://github.com/vcyzteen/microwave
+cd microwave
+rm -rf .git
+git init && git add . && git commit -m "Microwave" -s
+git push https://$GITHUB_TOKEN@github.com/vcyzteen/microwave.git
