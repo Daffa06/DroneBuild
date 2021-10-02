423c604995167c48f0f1b464e65408890d3dc11c
©TwT <vcyzscape@gmail.com>
2021-10-02 09:14:10 +0700

    Merged CAF

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # write your code in bellow
 git clone https://github.com/Baka-Project/Baka-Msm-4.4.git
 cd Baka-Msm-4.4
-curl https://raw.githubusercontent.com/Baka-Project/Baka-Merged/baka/baka-merged | bash
+git fetch https://source.codeaurora.org/quic/la/kernel/msm-4.4/ LA.UM.8.2.r2-04800-sdm660.0
+git merge FETCH_HEAD
 git push https://$GITHUB_TOKEN@github.com/Baka-Project/Baka-Msm-4.4.git
