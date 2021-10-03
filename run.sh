2d928f92933525e6bf661c0aae679ed203be72d6
©TwT <vcyzscape@gmail.com>
2021-10-03 18:11:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # write your code in bellow
-git clone https://github.com/vcyzteen/microwave
-cd microwave
-rm -rf .git
-git init && git add . && git commit -m "Microwave" -s
-git push https://$GITHUB_TOKEN@github.com/vcyzteen/microwave.git
+git clone https://$GITHUB_TOKEN@github.com/vcyzteen/module.git
+git clone https://github.com/somebodyshero/magisk-module-template
+cp -r magisk-module-template/* module
+git add . && git commit -m "initial upload" -s
+git push https://$GITHUB_TOKEN@github.com/vcyzteen/module.git
