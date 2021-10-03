659c82f496a0405879886030b586f1327f7f5e3c
©TwT <vcyzscape@gmail.com>
2021-10-03 18:12:25 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 git clone https://$GITHUB_TOKEN@github.com/vcyzteen/module.git
 git clone https://github.com/somebodyshero/magisk-module-template
 cp -r magisk-module-template/* module
+cd module
 git add . && git commit -m "initial upload" -s
 git push https://$GITHUB_TOKEN@github.com/vcyzteen/module.git
