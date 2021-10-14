c081f50515b46c06f6ae007a3c0252f95c9fff7c
©TwT <vcyzscape@gmail.com>
2021-10-14 22:50:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # write your code in bellow
 mkdir anime && cd anime
-wget -vO tkyravens.zip https://drive.google.com/u/0/uc?export=download&confirm=sWhL&id=1T0nVj-qxB9yOFNxOiv5kNN1hY0xGkUcx
+wget -O tkyravens.zip https://drive.google.com/u/0/uc?export=download&confirm=sWhL&id=1T0nVj-qxB9yOFNxOiv5kNN1hY0xGkUcx
 ls
 
