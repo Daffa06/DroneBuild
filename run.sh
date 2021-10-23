7f322d20822aa8b17a034af19124fa8f95f00db0
©TwT <vcyzscape@gmail.com>
2021-10-23 09:09:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 # make dir specific
 mkdir microwave && cd microwave
 # your code goes here
-pip install gdown
-gdown https://drive.google.com/uc?id=1T0nVj-qxB9yOFNxOiv5kNN1hY0xGkUcx&export=download
-ls -a
